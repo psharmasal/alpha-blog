6640123b986aa2db79fa44edc432915e650f1afa
Purushottam Sharma <p.sharma@salubrioustech.com>
2020-01-31 14:45:24 +0530

    Increse article character limit

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,6 @@ class Article < ApplicationRecord
 	has_many :article_categories
 	has_many :categories, through: :article_categories
 	
-	validates :title, presence: true, length: {minimum: 3, maximum: 50}
-	validates :description, presence: true, length: {minimum: 10, maximum: 300}
+	validates :title, presence: true, length: {minimum: 3, maximum: 200}
+	validates :description, presence: true, length: {minimum: 10, maximum: 3000}
 end
